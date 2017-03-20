94fd6a1886d14c59623628b10ade9e32cc399796
maki <marekmako@gmail.com>
2017-03-20 09:15:06 +0100

    Initial Commit

---
+++ DevVideoPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevVideoPlayer
+//
+//  Created by Marek Mako on 20/03/2017.
+//  Copyright © 2017 Marek Mako. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
